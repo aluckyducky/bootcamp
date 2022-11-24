c7b1df89c783192a441b4afa2b66848a0fa0f6fd
Alice <aliceliu@college.harvard.edu>
2022-11-23 23:13:53 -0500

    Added test cards, and a table to display them in CardEditor

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
 import logo from './logo.svg';
 import CardEditor from './CardEditor';
+import React from 'react';
 
-function App() {
-  return <CardEditor />;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      cards: [
+        { front: 'front1', back: 'back1'},
+        { front: 'front2', back: 'back2'},
+      ],
+    };
+  }
+
+  render() {
+    return <CardEditor cards={this.state.cards} />;
+  }
 }
 
 export default App;
